82e3478c2459a9a14cdd9a12812d28ef09b0abd7
dbswltn1 <yoonjisu1116@icloud.com>
2025-08-29 13:21:58 +0900

    2025 08 29

---
+++ chap02/src/sec04/exam03/QStopExample.java
@@ -0,0 +1,18 @@
+package sec04.exam03;
+
+public class QStopExample {
+
+	public static void main(String[] args) throws Exception {
+		int keyCode;
+		
+		while(true) {
+			keyCode = System.in.read();
+			System.out.println("keyCode: " + keyCode);
+			if(keyCode == 113) {
+				break;			//keyCode가 113일 경우(소문자 q) while을 중지함
+			}
+		}
+		System.out.println("종료");
+	}
+
+}
