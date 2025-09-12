ff45471c502af7695b95af0baf8df35ac8e6b53f
dbswltn1 <yoonjisu1116@icloud.com>
2025-09-12 22:07:13 +0900

    09-12

---
+++ chap06/src/sec02/exam01/Car.java
@@ -0,0 +1,13 @@
+package sec02.exam01;
+
+public class Car {
+
+	public static void main(String[] args) {
+		String company = "현대자동차";
+		String model = "그랜저";
+		String color = "검정";
+		int maxSpeed = 350;
+		int speed;
+	}
+
+}
