041ed826a3582f95329fb2ff74ca883027046dc2
dbswltn1 <yoonjisu1116@icloud.com>
2025-09-17 15:04:08 +0900

    09-17

---
+++ chap06/src/sec05/exam03/Singleton.java
@@ -0,0 +1,11 @@
+package sec05.exam03;
+
+public class Singleton {
+	private static Singleton singleton = new Singleton();
+	
+	private Singleton() {}
+	
+	static Singleton getInstance() {
+		return singleton;
+	}
+}
