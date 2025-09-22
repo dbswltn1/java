b487d54f547552abe2a26c654ab11f7e3f8fc658
dbswltn1 <yoonjisu1116@icloud.com>
2025-09-22 16:25:31 +0900

    09-22

---
+++ chap06/src/sec06/exam01/hyundai/Engine.java
@@ -0,0 +1,5 @@
+package sec06.exam01.hyundai;
+
+public class Engine {
+
+}
---
+++ chap06/src/sec06/exam01/mycompany/Car.java
@@ -0,0 +1,13 @@
+package sec06.exam01.mycompany;
+
+import sec06.exam01.hankook.*;
+import sec06.exam01.kumho.*;
+import sec06.exam01.hyundai.Engine;
+
+public class Car {
+	Engine engine = new Engine();
+	SnowTire tire1 = new SnowTire();
+	BigWideTire tire2 = new BigWideTire();
+	sec06.exam01.hankook.Tire tire3 = new sec06.exam01.hankook.Tire();
+	sec06.exam01.kumho.Tire tire4 = new sec06.exam01.kumho.Tire();
+}
